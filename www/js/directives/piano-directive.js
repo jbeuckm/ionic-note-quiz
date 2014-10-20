4ef6b4ca01fdbde86c73427de281ca452ebbc1c0
jbeuckm <joe@beigerecords.com>
2014-10-20 16:32:11 -0500

    converted to basic ionic app

---
+++ www/js/directives/piano-directive.js
@@ -0,0 +1,30 @@
+angular.module('note-quiz')
+    .directive('piano', [function(){
+        return {
+            restrict: 'E',
+            templateUrl: 'js/templates/piano-directive.html',
+            link: function(scope, element, attrs){
+                
+  // Create a piano, 700px wide and 200px high
+  var piano = new Clavier(700, 200);
+
+  // Append the piano element to the DOM
+  piano.appendTo(document.getElementById('piano-holder'));
+
+  var pressed = null;
+
+  piano.on('touchstart', function(e) {
+    pressed = e.target;
+    pressed.classList.add('pressed');
+  });
+
+  piano.on('touchend', function() {
+    if (pressed) {
+      pressed.classList.remove('pressed');
+      pressed = null;
+    }
+  });
+                
+            }
+        }
+    }]);
